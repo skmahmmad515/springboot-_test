5085a3dd97d5ea8691b526128877ac0d09ee5c51
user <user@DESKTOP-6DJII40>
2024-09-17 22:10:17 -0700

    first changes

---
+++ src/main/java/com/example/springboot_test/Homecontoller.java
@@ -0,0 +1,14 @@
+package com.example.springboot_test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Homecontoller {
+	
+	@GetMapping("/display")
+	public String diplay() {
+		return "javatpoint";
+	}
+
+}
